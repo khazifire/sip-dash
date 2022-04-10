1dacb53c5468a76e88a7c57cd764d35fab2ce4c7
khazifire <itskazimz@gmail.com>
2022-04-10 17:14:12 +0700

    updated admin dash (header, stats, table, sidebar)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
   content: ["./src/**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': "#0369a1",
+        'secondary': "#38bdf8",
+        'dark': "#3F3F46",
+        'mid': "#A1A1AA",
+        'light': "#F1F3F4",
+        transparent: 'transparent',
+        current: 'currentColor',
+      },
+    },
   },
   plugins: [],
 }
