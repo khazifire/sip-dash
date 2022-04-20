de15c9078438cfb7c315445dcf38c8b39bd6862b
khazifire <itskazimz@gmail.com>
2022-04-20 17:38:51 +0700

    updated navlink

--- components/PageLayout/NavBar.js
+++ components/PageLayout/NavBar.js
@@ -41,7 +41,7 @@ const Navbar = () => {
                         <svg className="w-8 h-8 sm:w-6 sm:h-6 fill-current " xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0z"/><path d="M3 13h8V3H3v10zm0 8h8v-6H3v6zm10 0h8V11h-8v10zm0-18v6h8V3h-8z"/></svg>
                         <span>Dashboard</span>
                     </a>
-                    <a href="/user/admin/customerList" className={router.pathname=="/user/admin/customerList"? "navlink-active":"navlink"}>
+                    <a href="/user/admin/userList" className={router.pathname=="/user/admin/userList"? "navlink-active":"navlink"}>
                         <svg className="w-8 h-8 sm:w-6 sm:h-6 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M2 22a8 8 0 1 1 16 0H2zm8-9c-3.315 0-6-2.685-6-6s2.685-6 6-6 6 2.685 6 6-2.685 6-6 6zm7.363 2.233A7.505 7.505 0 0 1 22.983 22H20c0-2.61-1-4.986-2.637-6.767zm-2.023-2.276A7.98 7.98 0 0 0 18 7a7.964 7.964 0 0 0-1.015-3.903A5 5 0 0 1 21 8a4.999 4.999 0 0 1-5.66 4.957z"/></svg>
                         <span>Users</span>
                     </a>
